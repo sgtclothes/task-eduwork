5bbdc476fa9ae7c88a055b33ecf8b5589fc324e3
Mohamad Ardiansyah Rofii <sayurelektronik@gmail.com>
2023-07-24 12:23:45 +0700

    create folder delete dan update

---
+++ ardiansyah/Delete dan Update/apotek.sql
@@ -0,0 +1,210 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 24 Jul 2023 pada 07.05
+-- Versi server: 10.4.25-MariaDB
+-- Versi PHP: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `apotek`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `karyawan`
+--
+
+CREATE TABLE `karyawan` (
+  `id_karyawan` int(11) NOT NULL,
+  `nama_karyawan` varchar(20) NOT NULL,
+  `alamat` text NOT NULL,
+  `no_telp` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `karyawan`
+--
+
+INSERT INTO `karyawan` (`id_karyawan`, `nama_karyawan`, `alamat`, `no_telp`) VALUES
+(3, 'siska', 'jl. hangtuah', '08563330639');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `obat`
+--
+
+CREATE TABLE `obat` (
+  `id_obat` int(11) NOT NULL,
+  `nama_obat` varchar(20) NOT NULL,
+  `harga` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `obat`
+--
+
+INSERT INTO `obat` (`id_obat`, `nama_obat`, `harga`) VALUES
+(1, 'Mixagrip', 3000),
+(2, 'oskadon', 2500),
+(3, 'paracetamol', 2500),
+(4, 'asamefenamat', 3500),
+(5, 'bodrex', 5000),
+(6, 'demacolin', 4500),
+(7, 'ponstan', 3000),
+(8, 'panadol', 6000),
+(9, 'decolgen', 3000),
+(10, 'diapet', 2500);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pembeli`
+--
+
+CREATE TABLE `pembeli` (
+  `id_pembeli` int(11) NOT NULL,
+  `nama_pembeli` varchar(20) NOT NULL,
+  `umur` int(11) NOT NULL,
+  `alamat` text NOT NULL,
+  `jenis_kelamin` enum('L','P') NOT NULL,
+  `id_obat` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `pembeli`
+--
+
+INSERT INTO `pembeli` (`id_pembeli`, `nama_pembeli`, `umur`, `alamat`, `jenis_kelamin`, `id_obat`) VALUES
+(1, 'ardiansyah', 25, 'jl. surabaya 45', 'L', 1),
+(2, 'nadhia', 25, 'jl. lumajang 77', 'P', 2),
+(3, 'adam', 45, 'jl. firdaus', 'L', 3),
+(4, 'soleh', 50, 'jl. manggis', 'L', 7),
+(5, 'hawa', 32, 'jl. mangga', 'P', 6),
+(6, 'marfuah', 56, 'jl. dukuh', 'P', 4),
+(7, 'yogi', 29, 'jjl. sukomanunggal', 'L', 5),
+(8, 'bayu', 28, 'jl. menganti', 'L', 2),
+(9, 'sinta', 66, 'jl. anggur', 'P', 3),
+(10, 'silvia', 47, 'jl. melon', 'P', 10);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id_transaksi` int(11) NOT NULL,
+  `id_pembeli` int(11) NOT NULL,
+  `id_obat` int(11) NOT NULL,
+  `id_karyawan` int(11) NOT NULL,
+  `tanggal_pembelian` date NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `total_bayar` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `transaksi`
+--
+
+INSERT INTO `transaksi` (`id_transaksi`, `id_pembeli`, `id_obat`, `id_karyawan`, `tanggal_pembelian`, `jumlah`, `total`, `total_bayar`) VALUES
+(8, 10, 9, 3, '2023-07-12', 1, 3000, 5000),
+(11, 5, 8, 3, '2023-07-13', 2, 5000, 5000),
+(12, 9, 7, 3, '2023-07-13', 5, 20000, 50000);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `karyawan`
+--
+ALTER TABLE `karyawan`
+  ADD PRIMARY KEY (`id_karyawan`);
+
+--
+-- Indeks untuk tabel `obat`
+--
+ALTER TABLE `obat`
+  ADD PRIMARY KEY (`id_obat`);
+
+--
+-- Indeks untuk tabel `pembeli`
+--
+ALTER TABLE `pembeli`
+  ADD PRIMARY KEY (`id_pembeli`),
+  ADD KEY `fk_pembeli` (`id_obat`);
+
+--
+-- Indeks untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id_transaksi`),
+  ADD KEY `fk_obat` (`id_obat`),
+  ADD KEY `fk_pembelii` (`id_pembeli`),
+  ADD KEY `fk_transaksi` (`id_karyawan`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `karyawan`
+--
+ALTER TABLE `karyawan`
+  MODIFY `id_karyawan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `obat`
+--
+ALTER TABLE `obat`
+  MODIFY `id_obat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT untuk tabel `pembeli`
+--
+ALTER TABLE `pembeli`
+  MODIFY `id_pembeli` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `pembeli`
+--
+ALTER TABLE `pembeli`
+  ADD CONSTRAINT `fk_pembeli` FOREIGN KEY (`id_obat`) REFERENCES `obat` (`id_obat`);
+
+--
+-- Ketidakleluasaan untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `fk_obat` FOREIGN KEY (`id_obat`) REFERENCES `obat` (`id_obat`),
+  ADD CONSTRAINT `fk_pembelii` FOREIGN KEY (`id_pembeli`) REFERENCES `pembeli` (`id_pembeli`),
+  ADD CONSTRAINT `fk_transaksi` FOREIGN KEY (`id_karyawan`) REFERENCES `karyawan` (`id_karyawan`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
