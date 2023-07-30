dd9f5ad0dd1271add52868a2e7805e4ec6e93f1b
Sigit <sigit.sasongko@locatorlogic.com>
2023-07-30 17:27:16 +0700

    masukin ke bangunRuang.js

--- modules/bangunRuang.js
+++ modules/bangunRuang.js
@@ -1,10 +1,13 @@
 import limas from "./limas.js";
+import persegi from "./persegi.js";
 
 let l = new limas();
+let p = new persegi();
 
 export default class bangunRuang {
     constructor() {
         this.limas = l;
+        this.persegi = p;
         this.bola = {
             keliling: () => {
                 return "rumus keliling bola";
