8495088380ddf8ae55b6ef9b68b904bbd1896bdf
wiratama13 <ashidiqinasrulloh@gmail.com>
2023-08-04 11:14:52 +0700

    php: array.php

--- wira/php/array/array.php
+++ wira/php/array/array.php
@@ -1,17 +1,8 @@
 <?php
 $no = 1;
-$nama_kelas = [
-    "Nama ke 1" => "Kelas 10",
-    "Nama ke 2" => "Kelas 9",
-    "Nama ke 3" => "Kelas 8",
-    "Nama ke 4" => "Kelas 7",
-    "Nama ke 5" => "Kelas 6",
-    "Nama ke 6" => "Kelas 5",
-    "Nama ke 7" => "Kelas 4",
-    "Nama ke 8" => "Kelas 3",
-    "Nama ke 9" => "Kelas 2",
-    "Nama ke 10" => "Kelas 1",
-];
+$array = file_get_contents('array.json');
+$data = json_decode($array, true);
+
 ?>
 
 <!doctype html>
@@ -20,7 +11,7 @@ $nama_kelas = [
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Looping 2</title>
+    <title>Array</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 </head>
 
@@ -31,13 +22,19 @@ $nama_kelas = [
                 <th scope="col">No</th>
                 <th scope="col">Nama</th>
                 <th scope="col">Kelas</th>
+                <th scope="col">Alamat</th>
+                <th scope="col">Tanggal lahir</th>
+                <th scope="col">nilai</th>
             </thead>
             <tbody>
-                <?php foreach ($nama_kelas as $nama => $kelas) : ?>
+                <?php foreach ($data as $key => $value) : ?>
                     <tr>
                         <td><?= $no++ ?></td>
-                        <td><?= $nama ?></td>
-                        <td><?= $kelas ?></td>
+                        <td><?= $value['nama'] ?></td>
+                        <td><?= $value['kelas'] ?></td>
+                        <td><?= $value['alamat'] ?></td>
+                        <td><?= $value['tanggal_lahir'] ?></td>
+                        <td><?= $value['nilai'] ?></td>
                     </tr>
                 <?php endforeach ?>
 
