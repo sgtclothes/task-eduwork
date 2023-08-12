5de7781bcd95b4a21826ceec76e87ea934a2a864
wiratama13 <ashidiqinasrulloh@gmail.com>
2023-08-12 07:13:47 +0700

    laravel: migrations and model

--- wira/laravel/database/migrations/2023_08_11_234012_create_transactions_table.php
+++ wira/laravel/database/migrations/2023_08_11_234012_create_transactions_table.php
@@ -11,15 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('transactions', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->unsignedBigInteger('member_id')->nullable();
             $table->foreign('member_id')->references('id')->on('members');
+            $table->date('date_start');
+            $table->date('date_end');
             $table->timestamps();
         });
     }
@@ -29,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('transactions');
     }
 };
