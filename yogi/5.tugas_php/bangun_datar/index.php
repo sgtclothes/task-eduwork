d30020670f97bb175da551eaf06ac7f90e8fa5c5
yogialfinaldo <yogialfinaldo990106@gmail.com>
2023-08-23 15:03:50 +0700

    upload

---
+++ yogi/5.tugas_php/bangun_datar/index.php
@@ -0,0 +1,14 @@
+<?php
+
+    require_once("Luas.php");
+
+    $luas = new Luas();
+    
+   #hitung  Luas Persegi Panjang
+     $p = 6;
+     $l = 5;
+     echo $luas->persegi_panjang($p,$l);
+
+    #output 20
+
+?>
