0ce3dd07b70e84df3f1095e97a50c2c565f267fc
Nara Yana <yananara996@gmail.com>
2023-08-26 11:01:37 +0700

    Operator

---
+++ narayana/PHP/Operator/bangun_datar.php
@@ -0,0 +1,35 @@
+<?php
+$a = 10;
+$b = 20;
+$c = 30;
+$d = 40;
+$Lebar = 10;
+$Tinggi = 15;
+$Panjang = 20;
+
+// Luas Persegi
+$e = $b + $b;
+echo "$b + $b = $e";
+echo "<hr>";
+
+// Keliling Persegi
+$e = 4 + $b;
+echo "4 + $b = $e";
+echo "<hr>";
+
+// Luas Persegi Panjang
+$e = $Panjang * $Lebar;
+echo "$Panjang * $Lebar = $e";
+echo "<hr>";
+
+// Keliling Persegi Panjang
+$e = 2 * $Panjang + 2 * $Lebar;
+echo "2 * $Panjang + 2 * $Lebar = $e";
+echo "<hr>";
+
+// Keliling Trapesium
+$e = $a + $b + $c + $d;
+echo "$a + $b + $c + $d = $e";
+echo "<hr>";
+echo "<br>";
+?>
---
+++ narayana/PHP/Operator/bangun_ruang.php
@@ -0,0 +1,24 @@
+<?php
+$a = 10;
+$b = 20;
+$c = 30;
+$d = 40;
+$Lebar = 10;
+$Tinggi = 15;
+$Panjang = 20;
+
+// Volume Kubus
+$e = $d * $a * $c;
+echo "$d * $a * $c = $e";
+echo "<hr>";
+
+// Volume Limas
+$e =(1/3) * $a * $b * $c;
+echo "(1/3) * $a * $b * $c = $e";
+echo "<hr>";
+
+// Volume Prisma
+$e =(1/2) * $a * $c * $d;
+echo "(1/2) * $a * $c * $d = $e";
+echo "<hr>";
+?>
