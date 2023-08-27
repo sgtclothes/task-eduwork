6ea966fb5e019c66e26b8695cc38eb15410fe064
rizalah <rizalahf@gmail.com>
2023-08-27 22:06:34 +0700

    add new task bangun datar dan ruang

---
+++ tugas/tugas_php/luas_bangun_datar.php
@@ -0,0 +1,36 @@
+<?php
+$a = 6;
+$b = 8;
+$d = 7;
+
+//luas persegi
+$c = $a * $a;
+echo "luas persegi <br>";
+echo "$a * $a = $c";
+echo "<hr>"; 
+
+//luas segitiga
+$c = 1/2 * $a * $a;
+echo "luas segitiga <br>";
+echo " 1/2 * $a * $b = $c";
+echo "<hr>";
+
+//luas lingkaran 
+$c = 3.14 * $a**2;
+echo "luas lingkaran <br>";
+echo " 3.14 * $a**2 = $c";
+echo "<hr>";
+
+//luas jajar genjang 
+$c = $a * $b;
+echo "luas jajar genjang <br>";
+echo " $a * $b = $c";
+echo "<hr>";
+
+//luas trapesium 
+$c = 1/2 * ($a + $b) * $d;
+echo "luas trapesium <br>";
+echo " 1/2 * ($a + $b) * $d = $c";
+echo "<hr>";
+
+?>
---
+++ tugas/tugas_php/volume_bangun_ruang.php
@@ -0,0 +1,24 @@
+<?php
+$a = 3;
+$b = 6;
+$d = 9;
+
+//volume kubus
+$c = $a * $a * $a;
+echo "volume kubus <br>";
+echo "$a * $a * $a = $c";
+echo "<hr>"; 
+
+// volume balok
+$c = $a * $b * $d ;
+echo "volume balok <br>";
+echo " $a * $b * $d = $c";
+echo "<hr>";
+
+//volume bola
+$c = 4/3 * 3.14 * $a**2;
+echo "volume bola <br>";
+echo " 4/3 * 3.14 * $a**2 = $c";
+echo "<hr>";
+
+?>
