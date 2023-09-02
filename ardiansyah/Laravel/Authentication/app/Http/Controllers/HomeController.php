ea0b3ac1cb02819634756fdbb5d2dbd1a6341d67
Mohamad Ardiansyah Rofii <sayurelektronik@gmail.com>
2023-09-03 03:07:25 +0700

    tugas membuat relasi

--- ardiansyah/Laravel/Authentication/app/Http/Controllers/HomeController.php
+++ ardiansyah/Laravel/Authentication/app/Http/Controllers/HomeController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Member;
+use App\Models\Book;
+use App\Models\Publisher;
+use App\Models\Catalog;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +27,11 @@ class HomeController extends Controller
      */
     public function index()
     {
+        // $members = Member::with('user')->get();
+        // $books = Book::with('publisher')->get();
+        // $books = Book::with('catalog')->get();
+        // $books = Book::with('author')->get();
+        
         return view('home');
     }
 }
--- ardiansyah/Laravel/Authentication/app/Models/Book.php
+++ ardiansyah/Laravel/Authentication/app/Models/Book.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     use HasFactory;
+
+    public function publisher()
+    {
+        # code...
+        return $this->belongsTo('App\Models\Publisher', 'publisher_id');
+    }
+
+    public function catalog()
+    {
+        # code...
+        return $this->belongsTo('App\Models\Catalog', 'catalog_id');
+    }
+
+    public function author()
+    {
+        # code...
+        return $this->belongsTo('App\Models\Author', 'author_id');
+    }
+
 }
