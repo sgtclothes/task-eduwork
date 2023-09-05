84b78533075a53392c7a92b0b78bd5bb9b6bc5da
Adi Tiya Wiranda <aditwiran19@gmail.com>
2023-09-05 10:19:38 +0700

    Materi 11

--- index-php.php
+++ index-php.php
@@ -3,14 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Test PHP</title>
 </head>
 <body>
     <script>
-
+        let a = "Hello adit" + " Hai";
+        // a += ' World';
+        console.log(a);
     </script>
+    
     <?php
-    $i = "hello";
+    $test = "Hello World2";
+    echo $test;
     
     ?>
 </body>
