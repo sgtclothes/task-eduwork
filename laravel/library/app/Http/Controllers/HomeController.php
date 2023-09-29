2a7649bd7373d612cd1c71c168f8045079e9b4a6
Keyzo1122 <subkhandimas02@gmail.com>
2023-09-30 01:23:26 +0700

    Membuat Relasi (Eloquent)

--- laravel/library/app/Http/Controllers/HomeController.php
+++ laravel/library/app/Http/Controllers/HomeController.php
@@ -2,6 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Member;
+use App\Models\Author;
+use App\Models\Catalog;
+use App\Models\Publisher;
+use App\Models\Book;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +28,10 @@ class HomeController extends Controller
      */
     public function index()
     {
+        // // $members = Member::with('user')->get();
+        // $books = Book::with('publisher')->get();
+
+        // return $books;
         return view('home');
     }
 }
--- laravel/library/database/seeders/MemberSeeder.php
+++ laravel/library/database/seeders/MemberSeeder.php
@@ -20,7 +20,7 @@ class MemberSeeder extends Seeder
             $member = new Member;
 
             $member->name = $faker->name;
-            $member->gender = $faker->randomElement($array = array('male', 'female'));
+            $member->gender = $faker->randomElement(['L', 'P']);
             $member->phone_number = '0821'.$faker->randomNumber(8);
             $member->address = $faker->address;
             $member->email = $faker->email;
