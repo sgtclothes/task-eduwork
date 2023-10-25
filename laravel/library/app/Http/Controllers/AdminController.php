182a40ad782d4241f0feda3d85cf9f3a65d3dbfb
Keyzo1122 <subkhandimas02@gmail.com>
2023-10-25 23:24:01 +0700

    Grafik

---
+++ laravel/library/app/Http/Controllers/AdminController.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use DB;
+use App\Buku;
+use App\Katalog;
+use App\Anggota;
+use App\Penerbit;
+use App\Pengarang;
+use App\Peminjaman;
+use App\Models\Admin;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function dashboard(){
+        $total_anggota = Anggota::count();
+        $total_buku = Buku::count();
+        $total_peminjaman = Peminjaman::whereMonth('tanggal_pinjam', date('m'))->count();
+        $total_penerbit = Penerbit::count();
+
+        $data_donut = Buku::select(DB::raw("COUNT(id_penerbit) as total"))->groupBy('id_penerbit')->orderBy('id_penerbit', 'asc')->pluck('total');
+        $label_donut = Penerbit::orderBy('penerbits.id', 'asc')->join('bukus','buku.id_penerbit','=','penerbits.id')->groupBy('nama_penerbit')->pluck('nama_penerbit');
+
+        $label_bar = ['Peminjaman', 'Pengembalian'];
+        $data_bar = [];
+
+        foreach ($label as $key => $value) {
+            $data_bar[$key]['label'] = $label_bar[$key];
+            $data_bar[$key]['backgroundColor'] = $key == 0 ? 'rgba(60,141,188,0.9)' : 'rgba(210, 214, 222, 1)';
+            $data_month = [];
+
+            foreach (range(1,12) as $month) {
+                if ($key == 0) {
+                    $data_month[] = Peminjaman::select(DB::raw("COUNT(*) as total"))->whereMonth('tgl_pinjam', $month)->first()->total;
+                } else {
+                    $data_month[] = Peminjaman::select(DB::raw("COUNT(*) as total"))->whereMonth('tgl_kembali', $month)->first()->total;
+                }
+            }
+
+            $data_bar[$key]['data'] = $data_month;
+        }
+        return view('admin.dashboard', compact('total_buku','total_anggota', 'total_peminjaman', 'total_penerbit', 'data_donut', 'label_donut', 'data_bar'));
+    }
+
+    public function katalog()
+    {
+        $data_katalog = Katalog::all();
+
+        return view('admin.katalog.katalog', compact('data_katalog'));
+    }
+    public function penerbit()
+    {
+        return view('admin.penerbit');
+    }
+    public function pengarang()
+    {
+        $data_pengarang = Pengarang::all();
+
+        return view('admin.pengarang', compact('data_pengarang'));
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Admin $admin)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Admin $admin)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Admin $admin)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Admin $admin)
+    {
+        //
+    }
+}
---
+++ laravel/library/resources/views/admin/dashboard.blade.php
@@ -0,0 +1,196 @@
+@extends('layouts.admin')
+
+@section('header', 'Dashboard')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-3 col-6">
+
+            <div class="small-box bg-info">
+                <div class="inner">
+                    <h3>{{ $total_buku }}</h3>
+                    <p>Total Buku</p>
+                </div>
+                <div class="icon">
+                    <i class="fa fa-book"></i>
+                </div>
+                <a href="{{ url('buku') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+
+        <div class="col-lg-3 col-6">
+
+            <div class="small-box bg-success">
+                <div class="inner">
+                    <h3>{{ $total_anggota }}</h3>
+                    <p>Total Anggota</p>
+                </div>
+                <div class="icon">
+                    <i class="fa fa-book"></i>
+                </div>
+                <a href="{{ url('anggota') }}" class="small-box-footer">More info <i
+                        class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+
+        <div class="col-lg-3 col-6">
+
+            <div class="small-box bg-warning">
+                <div class="inner">
+                    <h3>{{ $total_penerbit }}</h3>
+                    <p>Data Penerbit</p>
+                </div>
+                <div class="icon">
+                    <i class="fa fa-book"></i>
+                </div>
+                <a href="{{ url('penerbit') }}" class="small-box-footer">More info <i
+                        class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+
+        <div class="col-lg-3 col-6">
+
+            <div class="small-box bg-danger">
+                <div class="inner">
+                    <h3>{{ $total_peminjam }}</h3>
+                    <p>Data Peminjam</p>
+                </div>
+                <div class="icon">
+                    <i class="fa fa-book"></i>
+                </div>
+                <a href="{{ url('peminjaman') }}" class="small-box-footer">More info <i
+                        class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+
+        <div class="col-lg-6">
+            <div class="card card-danger">
+                <div class="card-header">
+                    <h3 class="card-title">Grafik Penerbit</h3>
+                    <div class="card-tools">
+                        <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                            <i class="fas fa-minus"></i>
+                        </button>
+                        <button type="button" class="btn btn-tool" data-card-widget="remove">
+                            <i class="fas fa-times"></i>
+                        </button>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <canvas id="donutChart"
+                        style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-6">
+            <div class="card card-success">
+                <div class="card-header">
+                    <h3 class="card-title">Grafik Peminjaman</h3>
+                    <div class="card-tools">
+                        <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                            <i class="fas fa-minus"></i>
+                        </button>
+                        <button type="button" class="btn btn-tool" data-card-widget="remove">
+                            <i class="fas fa-times"></i>
+                        </button>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div class="chart">
+                        <canvas id="barChart"
+                            style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+@endsection
+
+    <script src="{{ asset('assets../../plugins/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/chart.js/Chart.min.js') }}"></script>
+    <script type="text/javascript">
+        var label_donut = '{!! json_encode($label_donut) !!}';
+        var data_donut = '{!! json_encode($data_donut) !!}';
+        var $data_bar = '{!! json_encode($data_bar) !!}';
+
+        $(function() {
+            //-------------
+            //- DONUT CHART -
+            //-------------
+            // Get context with jQuery - using jQuery's .get() method.
+            var donutChartCanvas = $('#donutChart').get(0).getContext('2d')
+            var donutData = {
+                labels: JSON.parse('label_donut'),
+                datasets: [{
+                    data: JSON.parse('data_donut'),
+                    backgroundColor: ['#f56954', '#00a65a', '#f39c12', '#00c0ef', '#3c8dbc', '#d2d6de'],
+                }]
+            }
+            var donutOptions = {
+                maintainAspectRatio: false,
+                responsive: true,
+            }
+            //Create pie or douhnut chart
+            // You can switch between pie and douhnut using the method below.
+            new Chart(donutChartCanvas, {
+                type: 'doughnut',
+                data: donutData,
+                options: donutOptions
+            })
+            //-------------
+            //- BAR CHART -
+            //-------------
+            var areaChartData = {
+                labels: ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September',
+                    'Oktober', 'November', 'Desember'
+                ],
+                datasets: JSON.parse(data_bar)
+            }
+
+            var barChartCanvas = $('#barChart').get(0).getContext('2d')
+            var barChartData = $.extend(true, {}, areaChartData)
+            // var temp0 = areaChartData.datasets[0]
+            // var temp1 = areaChartData.datasets[1]
+            // barChartData.datasets[0] = temp1
+            // barChartData.datasets[1] = temp0
+
+            var barChartOptions = {
+                responsive: true,
+                maintainAspectRatio: false,
+                datasetFill: false
+            }
+
+            new Chart(barChartCanvas, {
+                type: 'bar',
+                data: barChartData,
+                options: barChartOptions
+            })
+            //---------------------
+            //- STACKED BAR CHART -
+            //---------------------
+            var stackedBarChartCanvas = $('#stackedBarChart').get(0).getContext('2d')
+            var stackedBarChartData = $.extend(true, {}, barChartData)
+
+            var stackedBarChartOptions = {
+                responsive: true,
+                maintainAspectRatio: false,
+                scales: {
+                    xAxes: [{
+                        stacked: true,
+                    }],
+                    yAxes: [{
+                        stacked: true
+                    }]
+                }
+            }
+
+            new Chart(stackedBarChartCanvas, {
+                type: 'bar',
+                data: stackedBarChartData,
+                options: stackedBarChartOptions
+            })
+        })
+    </script>
+
