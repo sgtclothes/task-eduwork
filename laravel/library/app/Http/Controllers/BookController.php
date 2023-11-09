084a76471b2e3d877e3461060d070a6b0e4f5dd6
Keyzo1122 <subkhandimas02@gmail.com>
2023-11-09 14:26:31 +0700

    studycase

--- laravel/library/app/Http/Controllers/BookController.php
+++ laravel/library/app/Http/Controllers/BookController.php
@@ -2,18 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Book;
 use App\Models\Author;
+use App\Models\Book;
 use App\Models\Catalog;
 use App\Models\Publisher;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
     /**
      * Display a listing of the resource.
      */
@@ -22,16 +18,21 @@ class BookController extends Controller
         $publishers = Publisher::all();
         $authors = Author::all();
         $catalogs = Catalog::all();
-        return view('admin.book', compact('publishers', 'authors', 'catalogs'));
+        return view(
+            'admin.book',
+            [
+                'publishers' => $publishers,
+                'authors' => $authors,
+                'catalogs' => $catalogs,
+            ]
+        );
     }
 
     public function api()
     {
         $books = Book::all();
-
         return json_encode($books);
     }
-
     /**
      * Show the form for creating a new resource.
      */
@@ -45,19 +46,50 @@ class BookController extends Controller
      */
     public function store(Request $request)
     {
-        $this->validate($request, [
-            'isbn' => ['required'],
-            'title' => ['required'],
-            'year' => ['required'],
-            'publisher_id' => ['required'],
-            'author_id' => ['required'],
-            'catalog' => ['required'],
-            'qty' => ['required'],
-            'price' => ['required'],
+        $data = $request->validate([
+            "isbn" => "required|string",
+            "title" => "required|email",
+            "year" => "required|numeric",
+            "publisher_id" => "required|numeric",
+            "author_id" => "required|numeric",
+            "catalog_id" => "required|numeric",
+            "qty" => "required|numeric",
+            "price" => "required|numeric",
         ]);
-        Book::create($request->all());
 
-        return redirect('books');
+        Book::create($data);
+        return redirect()->route('books.index');
+    }
+
+    public function apiStore()
+    {
+        $editStatus = request('editStatus');
+
+        if ($editStatus != "null") {
+            $book = Book::find($editStatus);
+            $book->isbn = request('isbn');
+            $book->title = request('title');
+            $book->year = request('year');
+            $book->publisher_id = request('publisher_id');
+            $book->author_id = request('author_id');
+            $book->catalog_id = request('catalog_id');
+            $book->qty = request('qty');
+            $book->price = request('price');
+            $book->save();
+        } else {
+            $book = new Book;
+            $book->isbn = request('isbn');
+            $book->title = request('title');
+            $book->year = request('year');
+            $book->publisher_id = request('publisher_id');
+            $book->author_id = request('author_id');
+            $book->catalog_id = request('catalog_id');
+            $book->qty = request('qty');
+            $book->price = request('price');
+            $book->save();
+        }
+
+        return response()->json('success', 200);
     }
 
     /**
@@ -76,33 +108,45 @@ class BookController extends Controller
         //
     }
 
+    public function apiGetBook($id)
+    {
+        $book = Book::find($id);
+
+        return response()->json($book, 200);
+    }
+
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Book $book)
+    public function update(Request $request, $id)
     {
-        $this->validate($request, [
-            'isbn' => ['required'],
-            'title' => ['required'],
-            'year' => ['required'],
-            'publisher_id' => ['required'],
-            'author_id' => ['required'],
-            'catalog' => ['required'],
-            'qty' => ['required'],
-            'price' => ['required'],
+        $data = $request->validate([
+            "isbn" => "required|string",
+            "title" => "required|email",
+            "year" => "required|numeric",
+            "publisher_id" => "required|numeric",
+            "author_id" => "required|numeric",
+            "catalog_id" => "required|numeric",
+            "qty" => "required|numeric",
+            "price" => "required|numeric",
         ]);
-        $book->update($request->all());
 
-        return redirect('books');
+        $validateData = $request->validate($data);
+        $author = Book::findOrFail($id);
+
+        $author->update($validateData);
+
+        return redirect()->route('authors.index');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Book $book)
-    {
-        $book->delete();
 
-        return redirect('books');
+    public function apiDelete($id)
+    {
+        $book = Book::findOrFail($id);
+        $book->delete();
+        return response()->json('success', 200);
     }
 }
--- laravel/library/app/Http/Controllers/MemberController.php
+++ laravel/library/app/Http/Controllers/MemberController.php
@@ -4,34 +4,37 @@ namespace App\Http\Controllers;
 
 use App\Models\Member;
 use Illuminate\Http\Request;
+use Yajra\DataTables\Facades\DataTables;
 
 class MemberController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
+        if ($request->gender) {
+            $datas = Member::where('gender', $request->gender)->get();
+            $datatables = datatables()->of($datas)->addIndexColumn();
+            return $datatables->make(true);
+        } else if (!$request->gender) {
+            $datas = Member::all();
+        }
+
         return view('admin.member');
     }
+
 
     public function api()
     {
         $members = Member::all();
-        $datatables = datatables()->of($members)
-        ->addColumn('date', function ($member) {
-            return convert_date($member->created_at);
-        })
-        ->addIndexColumn();
-
-        return $datatables->make(true);
+        $members = datatables()->of($members)
+            ->addColumn('date', function ($member) {
+                return convert_date($member->created_at);
+            })
+            ->addIndexColumn();
+        return $members->make(true);
     }
-
-
     /**
      * Show the form for creating a new resource.
      */
@@ -45,16 +48,16 @@ class MemberController extends Controller
      */
     public function store(Request $request)
     {
-        $this->validate($request, [
-            'name' => ['required'],
-            'phone_number' => ['required'],
-            'email' => ['required'],
-            'address' => ['required'],
-            'gender' => ['required'],
+        $data = $request->validate([
+            "name" => "required|string",
+            "gender" => "required|string|in:L,P",
+            "email" => "required|email",
+            "phone_number" => "required|numeric",
+            "address" => "required|string"
         ]);
-        Member::create($request->all());
 
-        return redirect('members');
+        Member::create($data);
+        return redirect()->route('members.index');
     }
 
     /**
@@ -76,27 +79,27 @@ class MemberController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Member $member)
+    public function update(Request $request, $id)
     {
-        $this->validate($request, [
-            'name' => ['required'],
-            'phone_number' => ['required'],
-            'email' => ['required'],
-            'address' => ['required'],
-            'gender' => ['required'],
+        $data = $request->validate([
+            "name" => "required|string",
+            "gender" => "required|string|in:L,P",
+            "email" => "required|email",
+            "phone_number" => "required|numeric",
+            "address" => "required|string"
         ]);
-        $member->update($request->all());
 
-        return redirect('members');
+        $members = Member::findOrFail($id);
+        $members->update($data);
+        return redirect()->route('members.index');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Member $member)
+    public function destroy($id)
     {
+        $member = Member::find($id);
         $member->delete();
-
-        return redirect('members');
     }
 }
