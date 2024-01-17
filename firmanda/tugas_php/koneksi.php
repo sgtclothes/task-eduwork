9e034069b49c3cbbc6d07670e9e2483d7767745a
Farseta <109111149+Farseta@users.noreply.github.com>
2024-01-18 02:13:40 +0700

    update for connection adding more query

--- firmanda/tugas_php/koneksi.php
+++ firmanda/tugas_php/koneksi.php
@@ -12,13 +12,26 @@ if(!$conn){
 }
 // echo "koneksi berhasil";
 // mysqli_close($conn);
-
+echo "query ke 1<br>";
 $sql = "SELECT * FROM buku";
 $result = $conn ->query($sql);
 
 if($result->num_rows >0){
     while($row = $result->fetch_assoc()){
         echo "buku : ".$row["isbn"]." ". $row["judul"]. "<br>";
+    }
+}
+else{
+    echo "0 result";
+}
+
+echo "<br>query ke 2 <br>";
+$sql2 = "SELECT * FROM anggota";
+$result2 = $conn ->query($sql2);
+
+if($result2->num_rows >0){
+    while($row = $result2->fetch_assoc()){
+        echo "anggota : ".$row["id_anggota"]." ". $row["nama"]. "<br>";
     }
 }
 else{
