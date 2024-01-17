bee20d25ed2573a0d591f36069c48989dfbb9ce7
Farseta <109111149+Farseta@users.noreply.github.com>
2024-01-17 09:31:12 +0700

    create new folder for tugas php and add for taks1

---
+++ firmanda/tugas_php/bangun_datar.php
@@ -0,0 +1,29 @@
+<?php
+echo "luas persegi panjang<br> ";
+$a = 5;
+$b = 4;
+$c = $a * $b ;
+echo"$a * $b = $c" ;
+echo"<hr>" ;
+echo "luas segitiga<br> ";
+$d = 4;
+$e = 6;
+$f =$d *$e / 2 ;
+echo"$d * $e / 2 = $f" ;
+echo"<hr>" ;
+echo "luas lingkaran<br>";
+$g = 10;
+$h =3.14 * $g * $g ;
+echo"3.14 * $g * $g = $h" ;
+echo"<hr>" ;
+echo "luas persegi<br>";
+$i = 11;
+$j = $i * $i ;
+echo"$i * $i = $j" ;
+echo"<hr>" ;
+echo "luas belah ketupat<br>";
+$k = 14;
+$l = $k * $k/2 ;
+echo"$k * $k / 2 = $l" ;
+echo"<hr>" ;
+?>
---
+++ firmanda/tugas_php/bangun_ruang.php
@@ -0,0 +1,22 @@
+<?php
+echo "volume balok<br> ";
+$a = 5;
+$b = 4;
+$i = 12;
+$c = $a * $b *$i ;
+echo"$a * $b * $i= $c" ;
+echo"<hr>" ;
+echo "volume piramida<br> ";
+$d = 4;
+$e = 6;
+$j = 11;
+$f =$d *$e * $j / 3 ;
+echo"$d * $e * $j / 3 = $f" ;
+echo"<hr>" ;
+echo "volume tabung<br>";
+$g = 10;
+$k = 7;
+$h =3.14 * $g * $g * $k ;
+echo"3.14 * $g * $g * $k = $h" ;
+echo"<hr>" ;
+?>
