0c0d09b20fadd8f8439a4981c975e505ae48c4bd
Farseta <109111149+Farseta@users.noreply.github.com>
2024-02-05 01:04:15 +0700

    adding for task eloquent realtionship model

--- firmanda/library/app/Http/Controllers/HomeController.php
+++ firmanda/library/app/Http/Controllers/HomeController.php
@@ -2,6 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Publisher;
+use App\Models\Member;
+use App\Models\Author;
+use App\Models\Book;
+use App\Models\catalog;
+
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +29,15 @@ class HomeController extends Controller
      */
     public function index()
     {
+        $books1 = Book::with('publisher')->get();
+        $books2 = Book::with('catalog')->get();
+        $books3 = Book::with('author')->get();
+        $members = Member::with('user')->get();
+        $publishers= publisher::with('books')->get();
+        $authors= Author::with('books')->get();
+        $catalogs= catalog::with('books')->get();
+
+        return $authors;
         return view('home');
     }
 }
